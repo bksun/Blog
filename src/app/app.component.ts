1ae6ee34df3e6d708a86f087b6226a9f544264ab
bksun <sunilsinghmbm@gmail.com>
2018-11-10 22:15:22 +0530

    create post and add to the accord list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Post } from './posts/post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Blog';
+
+  storedPosts: Post[] = [];
+
+  onPostAdded( post: Post ) {
+    this.storedPosts.push(post);
+  }
+
 }
